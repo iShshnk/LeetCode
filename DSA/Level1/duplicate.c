11b005c1c3eb2991d3b032cd71d4166bfdb23830
shashankrameshk <73171829+shashankrameshk@users.noreply.github.com>
2022-01-27 12:47:41 +0530

    Commit 1

---
+++ DSA/Level1/duplicate.c
@@ -0,0 +1,21 @@
+#include<stdio.h>
+#include<stdlib.h>
+int comp(const void *a, const void *b)
+{
+    return *(int *)a - *(int *)b;
+}
+
+bool containsDuplicate(int *nums, int numsSize)
+{
+    if (numsSize <= 1)
+        return 0;
+
+    qsort(nums, numsSize, sizeof(int), comp);
+
+    for (i = 1; i < numsSize; i++)
+    {
+        if (nums[i] == nums[i + 1])
+            return 1;
+    }
+    return 0;
+}
